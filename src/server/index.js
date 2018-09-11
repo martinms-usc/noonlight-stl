63f8ad4ab0838291e19108bc436189e831e99613
Michael Martin <martinms.usc@gmail.com>
2018-09-10 23:19:27 -0700

    deprecated test route

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ const os = require('os');
 
 const app = express();
 
+// serve webpack bundle
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+// app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(8080, () => console.log('Listening on port 8080!'));
