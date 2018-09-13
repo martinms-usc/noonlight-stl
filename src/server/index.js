53a6886b7c1ebb3e1883bd1db1ad2207fe561590
Michael Martin <martinms.usc@gmail.com>
2018-09-12 23:05:00 -0700

    remove unneeded server code

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const os = require('os');
-
 const app = express();
 
 // serve webpack bundle
 app.use(express.static('dist'));
 
-// app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(8080, () => console.log('Listening on port 8080!'));
