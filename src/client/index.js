f4bc801a87e8c26f5205d58a14d1edac6abeb789
Michael Martin <martinms.usc@gmail.com>
2018-09-20 16:55:43 -0400

    react router not needed for now

--- src/client/index.js
+++ src/client/index.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import document from 'global/document';
-import { hashHistory, Router, Route } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
 import store from './store';
 import App from './app';
 
-const history = syncHistoryWithStore(hashHistory, store);
+// to enable react-router and view routing
+// import { hashHistory, Router, Route } from 'react-router';
+// import { syncHistoryWithStore } from 'react-router-redux';
+// const history = syncHistoryWithStore(hashHistory, store);
 
 const Root = () => (
   <Provider store={store}>
-    <Router history={history}>
-      <Route path='/' component={App} />
-    </Router>
+    <App />
   </Provider>
 )
+
+// const Root = () => (
+//   <Provider store={store}>
+//     <Router history={history}>
+//       <Route path='/' component={App} />
+//     </Router>
+//   </Provider>
+// )
 
 render(<Root />, document.getElementById('root'));
