1584655b87d20b245c0b6a80e980901c1327fa0d
Michael Martin <martinms.usc@gmail.com>
2018-09-20 16:54:08 -0400

    updated app with config swap actions

--- src/client/App.js
+++ src/client/App.js
@@ -1,76 +1,57 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
 
 import KeplerGl from 'kepler.gl';
-import Processors from 'kepler.gl/processors';
-import {addDataToMap, toggleSidePanel} from 'kepler.gl/actions';
+import { addDataToMap, toggleSidePanel } from 'kepler.gl/actions';
 import KeplerGlSchema from 'kepler.gl/schemas';
 
-import noonlightUsage from '../data/noonlightTrips.csv';
-import config from '../data/config';
+import { loadConfig, downloadJsonFile } from './actions';
 
-import nlStacked from '../../public/nl_stacked.png';
-import typemark from '../../public/nl_white_typemark.png';
-
-import downloadJsonFile from "./file-download";
+import typemark from '../../public/typemark.png';
+import heatmap from '../../public/heatmap.png';
+import hexbin from '../../public/hexbin.png';
 import './app.css';
-
-
-const styles = {
-  container: {
-    borderRadius: "5px",
-    border: 0,
-    background: "#344351",
-    zIndex: 9,
-    position: "absolute",
-    display: "block",
-    bottom: "0px",
-    left: "5px",
-    position: "absolute",
-    display: "block",
-    width: "219px",
-    height: "27px"
-  },
-  typemark: {
-    bottom: "4px",
-    position: "absolute",
-    zIndex: 10,
-    width: "200px",
-    cursor: "pointer",
-    left: "9px",
-  }
-}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.visitNoonlight = this.visitNoonlight.bind(this);
+    this.exportMapConfig = this.exportMapConfig.bind(this);
+    this.addData = this.addData.bind(this);
+    this.selectConfig = this.selectConfig.bind(this);
+    this.selectHexbin = this.selectHexbin.bind(this);
+    this.selectHeatmap = this.selectHeatmap.bind(this);
   }
 
   componentDidMount() {
-    const data = Processors.processCsvData(noonlightUsage);
-    const dataset = {
-      data,
-      info: {
-        id: 'my_data',
-        label: 'Noonlight Trips'
-      }
-    };
-    this.props.dispatch(
-      addDataToMap({
-        config,
-        datasets: dataset 
-      })
-    );
+    // console.log(this.props);
     this.props.dispatch(
       toggleSidePanel()
+    );
+    this.selectConfig('hexbin');
+  }
+
+  selectConfig(type) {
+    this.props.dispatch(
+      this.props.loadConfig(type)
+    );
+    this.addData();
+  }
+
+  addData() {
+    this.props.dispatch(
+      addDataToMap({
+        config: this.props.config.data,
+        datasets: this.props.dataset 
+      })
     );
   }
 
   getMapConfig() {
-    const {keplerGl} = this.props;
-    const {map} = keplerGl;
+    const { keplerGl}  = this.props;
+    const { map } = keplerGl;
     return KeplerGlSchema.getConfigToSave(map);
   }
 
@@ -84,29 +65,79 @@ class App extends Component {
     win.focus();
   }
 
+  selectHeatmap() {
+    this.selectConfig('heatmap');
+  }
+
+  selectHexbin() {
+    this.selectConfig('hexbin');
+  }
+
   render() {
-// <button onClick={this.exportMapConfig}>Export</button>
     return (
       <div style={{position: 'absolute', width: '100%', height: '100%'}}>
-        <div onClick={this.visitNoonlight} style={styles.container}>
-          <img src={typemark} style={styles.typemark} />
+        <div onClick={this.visitNoonlight} className='container'>
+          <img src={typemark} className='typemark' />
         </div>
+        
         <AutoSizer>  
-          {(height, width) => (
+          {({height, width}) => (
             <KeplerGl
-              mapboxApiAccessToken={MAPBOX_TOKEN}
+              mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
               id="map"
               width={width}
               height={height}
             />
           )}
         </AutoSizer>
+        <button className='export' onClick={this.exportMapConfig}>Export</button>
+        {
+          this.props.config.selected === 'hexbin' ? 
+          (<Heatmap selectHeatmap={this.selectHeatmap}/>) :
+          (<Hexbin selectHexbin={this.selectHexbin} />)
+        }
       </div>
     );
   }
 }
 
-const mapStateToProps = state => state;
-const dispatchToProps = dispatch => ({dispatch});
+function Heatmap(props) {
+  return (
+    <div onClick={props.selectHeatmap} className='config-container'>
+      <span className='label heat'>Heat Map</span>
+      <img src={heatmap} className='config-select' />
+    </div>
+  );
+}
+
+function Hexbin(props) {
+  return (
+    <div onClick={props.selectHexbin} className='config-container'>
+      <span className='label hex'>Hex Bin</span>
+      <img src={hexbin} className='config-select' />
+    </div>
+  );
+}
+
+App.propTypes = {
+  visitNoonlight: PropTypes.func,
+  exportMapConfig: PropTypes.func,
+  selectHexbin: PropTypes.func,
+  selectHeatmap: PropTypes.func,
+  keplerGl: PropTypes.object,
+  dataset: PropTypes.object,
+  config: PropTypes.object
+};
+
+const mapStateToProps = state => ({
+  keplerGl: state.keplerGl,
+  dataset: state.app.dataset,
+  config: state.app.config
+});
+
+const dispatchToProps = dispatch => ({
+  dispatch,
+  loadConfig
+});
 
 export default connect(mapStateToProps, dispatchToProps)(App);
