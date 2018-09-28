1ab98cdbfe67a7a9dd36e797c64c55a89962f709
Michael Martin <martinms.usc@gmail.com>
2018-09-28 12:07:55 +0100

    semicolon added

--- src/server/index.js
+++ src/server/index.js
@@ -4,5 +4,5 @@ const app = express();
 // serve webpack bundle
 app.use(express.static('dist'));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Listening on port 8080'));
